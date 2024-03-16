883765da9516d1f8ed24f8e25eb256edbcc2f10c
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-03-17 04:56:03 +0600

    Start Logical Operators

--- part_2/app.js
+++ part_2/app.js
@@ -50,7 +50,9 @@
 // Comparison Operators 
 
 // == === != !== > < >= <=
-let a = 5;
-let b = "3";
+// let a = 5;
+// let b = "3";
 
-console.log("a >= b ", a >= b);
+// console.log("a >= b ", a >= b);
+
+// Logical Operators
