3de30416cf8923315beffc807b1e530081b860ac
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-03-17 04:39:56 +0600

    start Comparison Operators

--- part_2/app.js
+++ part_2/app.js
@@ -26,5 +26,27 @@
 // Assignmet Operators
 // = += -= *= %= **=
 
-let a = 5;
-let b = 2;
+// let a = 5;
+// let b = 2;
+// a+=4; // a = a + 4;
+// console.log("a = ", a);
+
+// b-= 1; // b = b - 1;
+// console.log("b =", b);
+
+// a*=2; // a = a * 2;
+
+// console.log("a =", a);
+
+// b/= 2; // b = b / 2;
+// console.log("b =", b);
+
+// a%= 4; // a = a % 4;
+// console.log("a =", a);
+
+// a**=4; // a = a **4;
+// console.log("a =", a)
+
+// Comparison Operators 
+
+// == === != !== > < >= <=
