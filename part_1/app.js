1f85abf68bdf91ae1644a9d0418bc07ad88d0ab8
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-03-16 21:22:06 +0600

    complet part_1

--- part_1/app.js
+++ part_1/app.js
@@ -1,7 +1,17 @@
-const product = {
-  title: "Ball Pen",
-  rating: 4,
-  offer: 5,
-  price: 270
+// const product = {
+//   title: "Ball Pen",
+//   rating: 4,
+//   offer: 5,
+//   price: 270
+// }
+// console.log(product);
+
+const profile = {
+  username: "@abdulsalam",
+  isFollow: false,
+  followers: 123,
+  following: 45
 }
-console.log(product);
+console.log(typeof profile["username"]);
+console.log(typeof profile["followers"]);
+console.log(typeof profile["isFollow"]);
