2eb4580b7ac9ca17608f4eb0493bce9f1fc13f9d
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-03-16 22:04:16 +0600

    start Assign Operators

--- part_2/app.js
+++ part_2/app.js
@@ -1,1 +1,27 @@
 // Artimetic Operators
+// + - * / % ++ -- ** 
+// let a = 5;
+// let b = 2;
+// let c = a + b;
+// console.log(" a + b = ", c);
+// console.log(" a - b = ", a - b);
+// console.log("a * b =", a * b);
+// console.log("a = ", a, "&", "b =", b);
+// console.log("a / b =", a/b);
+// console.log("a % b =", a%b);
+// console.log("a ** b =", a ** b);
+
+
+// Unary Operators
+// let a = 5;
+// let b = 2;
+// a++;
+// console.log("a =", a, " & b = ", b);
+// console.log("a = ", a);
+// a--;
+// console.log("a = ", a);
+
+
+
+// Assign Operators
+// = += -= *= %= **=
