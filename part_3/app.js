e5e44e3250c8e3acb09b0bd4ab789c413458df8b
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-03-19 19:00:48 +0600

    Start prompt

--- part_3/app.js
+++ part_3/app.js
@@ -1,3 +1,19 @@
-for(let i = 1; i<= 10; i++){
-  console.log("My name is Abdul Salam");
-}
+// Loops in js
+//for loop
+// for(let i = 1; i<= 10; i++){
+//   console.log("My name is Abdul Salam");
+// }
+
+// Calculate sum of 1 to 5
+// let sum = 0;
+// for (let i = 1; i <= 5; i++){
+//   sum = sum + i;
+// }
+// console.log(sum);
+
+// let sum = 0;
+// let n = prompt("Enter Your Number :");
+// for (let i = 1; i<= n; i++){
+//   sum = sum + i;
+// }
+// console.log(sum)
