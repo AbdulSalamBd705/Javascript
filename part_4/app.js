6c31b49235f8091253d8daf5360ab7ecd142a52d
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-03-22 05:25:17 +0600

    CharAt

--- part_4/app.js
+++ part_4/app.js
@@ -1,1 +1,25 @@
 // JavaScript String Methods
+
+// JavaScript String Length 
+// let text = "My Name Is Abdul Salam";
+// console.log(text.length);
+
+// let name = "Hello, I'm a Abdul Salam. I'm From Dhaka Bangladesh";
+
+// console.log(name.length)
+
+// let salam = {
+//   fristName: "MD ABDUL",
+//   lastName: "SALAM",
+//   age: 20,
+//   key: 12,
+// };
+// console.log(salam.fristName.length);
+
+// JavaScript String charAt
+// let text = "HELLO WORLD";
+// console.log(text.charAt(0));
+
+// let text = "My Name Is Abdul Salam";
+// console.log(text.charAt(1));
+
