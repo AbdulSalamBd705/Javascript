749d680b0b8fbd0a2fcd1339daa4d4cab069fe71
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-03-22 20:44:11 +0600

    for in loop

--- part_3/app.js
+++ part_3/app.js
@@ -71,3 +71,12 @@
 
 // for in loop
 
+// let student = {
+//   name: "Rahul Kumar",
+//   age: 20,
+//   cgpa: 7.5,
+//   isPass: true
+// };
+// for(let i in student){
+//   console.log(i);
+// };
