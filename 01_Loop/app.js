e18282ccd2e4f8779fee141e1b162a4eeeb34c32
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-03-27 04:35:13 +0600

    While Loop

---
+++ 01_Loop/app.js
@@ -0,0 +1,70 @@
+// // For loops 
+// const cars = ["A", "B", "C", "D", "E"];
+
+// for(let i = 0; i < cars.length; i++){
+//   console.log(i);
+// }
+
+// const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+// let text = "";
+
+// for(let i = 0; i < cars.length; i++){
+//   console.log(text +=cars[i])
+// };
+
+// let text = "*";
+// for(let i = 0; i < 20; i++){
+//   console.log(text)
+// }
+// const cars = ["Volvo", "Saab", "Ford", "Fiat", "Audi"];
+// let text = "";
+// for(let i = 0; i < cars.length; i++){
+//   console.log(text += cars[i]);
+// };
+
+// let text = "";
+// for(let i = 0; i <= 10; i++){
+//   console.log(i)
+// }
+
+
+// For in Loops 
+// const person = {fname: "John", lname: "Doe", age: 21};
+
+// for(let i in person){
+//   console.log(person[i]);
+// };
+
+
+// const salam = {fname: "Md Abdul", lname: "Salam", age: 21};
+
+// for(let i in salam){
+//   console.log(salam[i]);
+// };
+
+// const kona = {fname: "Kona", lname: "Abdul Salam", age:21};
+// for(let i in kona){
+//   console.log(kona[i])
+// }
+
+// const name = {salam: "MD ABDUL SALAM",salamage: 21, kona: "MST KONA", konaage: 17 };
+
+// for(let i in name){
+//   console.log(name[i]);
+// };
+
+// const person = {
+//   name: "Abdul Salam",
+//   age: 21,
+//   cgpa: 4.22
+// }
+// for(let i in person){
+//   console.log(person[i])
+// }
+
+
+// for of Loop 
+// let a =[4,5,6,7,8,9,];
+// for(let x of a){
+//   console.log(x);
+// };
