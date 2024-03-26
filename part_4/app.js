d0e3b1195151c1fcc623d55eb089917253d9c9dc
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-03-26 19:39:27 +0600

    Start

--- part_4/app.js
+++ part_4/app.js
@@ -7,5 +7,19 @@
 // let love = ["Kona",'Hasina', "Saidul", "Sazina", "Shamim", "Huyaira", "Abu Said"];
 
 // console.log(typeof love)
-let marks = [12, 23,34,45,67,78];
-console.log(marks[2])
+// let marks = [12, 23,34,45,67,78];
+// // console.log(marks[2]="Salam")
+// for (let i = 0; i < marks.length; i++){
+//   console.log(marks[i])
+// }
+// let marks = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+// for(let i = 0; i < marks.length; i++){
+//   console.log(marks[i]);
+// };
+
+
+// Arrays 
+// let name = ['Salam', 'Kona', "Saidul", 'Hasina', "Samim", "Sazina", "Humayra", "Abu Said"];
+// for(let index = 0; index < name.length; index++){
+//   console.log(name[index]);
+// };
