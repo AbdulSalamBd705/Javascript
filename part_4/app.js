4adb60065d9d24a2f3e2ee0fd936018e81e510ae
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-03-26 19:23:33 +0600

    Satrt Array

--- part_4/app.js
+++ part_4/app.js
@@ -1,28 +1,11 @@
-// JavaScript String Methods
+// Arrays in Js
+// Collections of Items
+// let student = [1,2,3,4,5,6,7,8,9,10];
+// console.log(student);
+// console.log(student.length);
 
-// JavaScript String Length 
-// let text = "My Name Is Abdul Salam";
-// console.log(text.length);
+// let love = ["Kona",'Hasina', "Saidul", "Sazina", "Shamim", "Huyaira", "Abu Said"];
 
-// let name = "Hello, I'm a Abdul Salam. I'm From Dhaka Bangladesh";
-
-// console.log(name.length)
-
-// let salam = {
-//   fristName: "MD ABDUL",
-//   lastName: "SALAM",
-//   age: 20,
-//   key: 12,
-// };
-// console.log(salam.fristName.length);
-
-// JavaScript String charAt
-// let text = "HELLO WORLD";
-// console.log(text.charAt(0));
-
-// let text = "My Name Is Abdul Salam";
-// console.log(text.charAt(1));
-
-// JavaScript String charDodeAt();
-let text = "HELLO WORLD";
-console.log(text.charCodeAt(1));
+// console.log(typeof love)
+let marks = [12, 23,34,45,67,78];
+console.log(marks[2])
