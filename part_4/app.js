a33865e186d9545d7713cc48be540c2df1dcaa7a
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-03-28 05:23:29 +0600

    Array

--- part_4/app.js
+++ part_4/app.js
@@ -72,3 +72,69 @@
 // Let's Practice 
 // Qs. For a given array with prices of 5 items -> [250, 645, 300, 900, 50]
 // All items have an offer of 10% off them. Chem. Change the Array to store final price after applying offer 
+
+
+// let items = [250, 645, 300, 900, 50];
+// let i = 0;
+// for(let val of items){
+//   let offer = val / 10;
+//   items[i] = items[i] - offer;
+//   console.log(`value after offer = ${items[i]}`);
+//   i++;
+// }
+
+// for(let i = 0; i < items.length; i++){
+//   let offer = items[i] / 10;
+//   items[i] -= offer;
+// }
+// console.log(items)
+
+// Arrays in js 
+// Array Methods
+// let foodItems = ["potato", "Apple", "Litchi", "Tomato"];
+// foodItems.push("Chips", "Burger", "Panner");
+
+// console.log(foodItems);
+
+// let foodItems = ["Potato", "Apple", "Litchi", "Tomato"];
+
+// console.log(foodItems);
+// let deletedItems = foodItems.pop();
+// console.log(foodItems);
+
+// console.log(`deleted ${deletedItems}`);
+
+// let foodItems = ["Potato", "Apple", "Litchi", "Tomato"];
+
+// console.log(foodItems);
+
+// console.log(foodItems.toString());
+
+// Concat();; Joins Multiple Arrays & returns result
+// let marvelHeroes = ["thor", "spiderman", "Ironman"];
+// let deHeroes = ["Superman", "barman"];
+
+// let heroes = marvelHeroes.concat(deHeroes);
+
+// console.log(heroes);
+
+
+// Unshift Add to start 
+// let marvelHeroes = ["Thor", "Spiderman", "Ironman"];
+
+// marvelHeroes.unshift("Antman");
+
+// console.log(marvelHeroes)
+
+
+// Shift 
+// let marvelheroes = ["Thor", "Spiderman", "Ironman"];
+// console.log(marvelheroes);
+
+// let val = marvelheroes.shift();
+// console.log(`deleted ${val}`);
+
+let marvelHeroes = ["thor", "spiderman", "ironman", "antman", "Dr. Strange"];
+
+console.log(marvelHeroes);
+console.log(marvelHeroes.slice(1, 3));
