53ad03e0fed9b8a148ac2ce1dee3831cde681887
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-03-29 11:53:45 +0600

    Arrow Function

--- part_5/app.js
+++ part_5/app.js
@@ -1,5 +1,36 @@
-function myFunction(){
-  console.log("Welcome to Crazy Programming ");
-  console.log("We are Learning JS")
-}
-myFunction();
+// function myFunction(){
+//   console.log("Welcome to Crazy Programming ");
+//   console.log("We are Learning JS")
+// }
+// myFunction();
+
+// function myFunction(msg, n){
+//   // parameter -> input
+//   console.log(msg * n);
+// }
+// myFunction("Welcome to crazy programer", 100); // argument
+
+// Function -> 2 numbers, sum
+// function sum(number1, number2){
+//   console.log(number1 + number2);
+// }
+
+// sum(12, 13)
+
+// function sum(x, y){
+//   s = x + y;
+//   return s;
+// }
+// let val = sum(12, 13);
+// console.log(val)
+
+// function functionName(param1, param1....){
+  // do some work
+//}
+
+
+//Arrow Functions 
+// Compact way of writing a function 
+// const functionName = (Param1, param2....)=>{
+  // do some work
+// }
