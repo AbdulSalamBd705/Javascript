bcfb5a18e570587c67fb1a7bc8e7a93b4e540ee4
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-03-30 05:09:53 +0600

    addEventsLiser

--- part_7/app.js
+++ part_7/app.js
@@ -1,11 +1,30 @@
-let btn1 = document.querySelector("#btn1");
+// let btn1 = document.querySelector("#btn1");
 
-btn1.onclick = (e)=>{
-  console.log(e)
-}
+// btn1.onclick = (e)=>{
+//   console.log(e)
+// }
 
-let box = document.querySelector("#box");
+// let box = document.querySelector("#box");
 
-box.onmouseover = ()=>{
-  console.log("you are inside div")
-}
+// box.onmouseover = ()=>{
+//   console.log("you are inside div")
+// }
+
+// Events Listeners
+
+// addEventListener(event, callback)
+// removeEventListener(event, callback)
+
+// let btn1 = document.querySelector("#btn1");
+
+// btn1.addEventListener("click", (evt)=>{
+//   console.log("button1 was clicked");
+//   console.log(evt);
+// })
+
+// const handler3 = ()=>{
+//   console.log("button2 was clicked - hendler3")
+// }
+// btn1.addEventListener("click", handler3);
+
+// btn1.removeEventListener("click", handler3);
