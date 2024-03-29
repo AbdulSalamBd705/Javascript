e9874407bcfe4cd2a0ddb1a3ce2b7366504253ef
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-03-30 04:56:30 +0600

    Events

---
+++ part_7/app.js
@@ -0,0 +1,11 @@
+let btn1 = document.querySelector("#btn1");
+
+btn1.onclick = (e)=>{
+  console.log(e)
+}
+
+let box = document.querySelector("#box");
+
+box.onmouseover = ()=>{
+  console.log("you are inside div")
+}
